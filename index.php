afdad2443b041dbbd2bcfb8f2661af8c14b61563
osama779211010 <www.osama779211010@gmail.com>
2026-03-12 03:14:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // This script serves the frontend dashboard to make it compatible with Railway's PHP buildpack
-header('Location: MAS_WEB/frontend/login.html');
+header('Location: MAS_WEB/frontend/index.html');
 exit;
+
 
